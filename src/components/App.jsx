20a40d56bd9cfc8d0f627dd191f523245a2bc163
Dazavrrr <damelasha@gmail.com>
2023-10-09 22:19:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import user from '../user.json';
+import { Profile } from './profile/Profile';
+import { GlobalStyle } from './GlobalStyle';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile items={user} />
+      
+
+      <GlobalStyle />
     </div>
   );
 };
